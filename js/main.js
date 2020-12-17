6f0a587c298ff980377baa768ac7c3af917fca3d
Jurate <jurate.jastromskaite@gmail.com>
2020-12-17 12:24:55 +0200

    Object #1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// OBJEKTAS
+// - parametru rinkinys
+//- funkcionalumo rinkinys
+
+
+/*
+
+Animal.js (params: name, color, sound; methods: voice();)
+    Pet.js  (viska paveldi is Animal.js)
+        dog.js     (viska paveldi is Pet.js)
+        cat.js     (viska paveldi is Pet.js)
+        hamster.js (viska paveldi is Pet.js; params: canEatTillDeath)
+    
+    Bird.js   (viska paveldi is Animal.js; params: wingsCount, canFly; methods: flyAway();)
+        chicken.js   (viska paveldi is bird.js)
+        parrot.js    (viska paveldi is bird.js)
+        eagle.js     (viska paveldi is bird.js)
+        dragonfly.js (viska paveldi is bird.js)
+    
+    Fish.js (viska paveldi is Animal.js; params: canSwim; methods: swimAway(); )
+        Goldfish.js  (viska paveldi is Animal.js; params: methods: makeWish(); )            
+        nemo.js (viska paveldi is bird.js)
+
+
+
+*/
